4c70ae7dfe1aafbf74aaa022284b3408c50a35dc
David Salinas Fernández de Landa <d.salinas@ibermatica.com>
2023-05-09 10:27:52 +0200

    Regeneración base de datos tras limpieza modelos entity.

--- Modelos/Entidades/Libro.cs
+++ Modelos/Entidades/Libro.cs
@@ -1,6 +1,7 @@
 ﻿using Modelos.Validaciones;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Modelos.Entidades
     {
         public int Id { get; set; }
         [PrimeraLetraMayuscula]
+        [StringLength(maximumLength:250)]
         public string Titulo { get; set; }
         public int AutorId { get; set; }
         public Autor? Autor { get; set; }
